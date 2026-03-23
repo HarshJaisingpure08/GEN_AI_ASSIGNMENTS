55d362cfc91909a384bf8cfa1497654d71d2ae86
Harsh Jaisingpure <jaisingpure.harsh08@gmail.com>
2026-03-23 13:17:34 +0530

    Initial commit of assignments

---
+++ Assignment_1/1_assignment.py
@@ -0,0 +1,20 @@
+import cohere
+
+co = cohere.Client("YOUR_COHERE_API_KEY")
+
+response = co.chat(
+    model="command-r-plus", 
+    temperature=0.7,
+    messages=[
+        {
+            "role": "system",
+            "content": "Talk like Joe Goldberg from YOU"
+        },
+        {
+            "role": "user",
+            "content": "Write a Bollywood movie plot in 5 sentences"
+        }
+    ]
+)
+
+print(response.text)
